bd574b67d306a4d119458d61296d74bfde70a15b
prad-j <58523204+prad-j@users.noreply.github.com>
2019-12-10 01:04:02 +0530

    Chng01

--- Test01/src/Prad01/Hello.java
+++ Test01/src/Prad01/Hello.java
@@ -2,7 +2,7 @@ package Prad01;
 
 public class Hello {
 	public static void main(String args[]){
-		System.out.println("Hello Eclipse and Git!!!");
+		System.out.println("Hello Eclipse & Git!!!");
 	}
 
 }
